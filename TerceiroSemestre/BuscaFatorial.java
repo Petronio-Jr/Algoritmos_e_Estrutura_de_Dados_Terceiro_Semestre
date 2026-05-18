7825232d5b013a789ce283ee13e167676e74cef2
Pet <petronio.junior@sou.unifal-mg.edu.br>
2026-05-18 16:03:10 -0300

    Delete TerceiroSemestre directory

--- TerceiroSemestre/BuscaFatorial.java
+++
@@ -1,9 +0,0 @@
-package TerceiroSemestre;
-
-public class BuscaFatorial {
-    public static void main(String[] args){
-        Fatorial valor = new Fatorial();
-        int resultado = valor.fat(5);
-        System.out.println(resultado);
-    }
-}
--- TerceiroSemestre/Fatorial.java
+++
@@ -1,8 +0,0 @@
-package TerceiroSemestre;
-
-public class Fatorial {
-    public int fat(int n){
-        if(n == 0) return 1;
-        return n * fat(n-1);
-    }
-}
